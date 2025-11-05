6067ad612fa33d9df65bc3d2fa3ef0dd9c2272e2
Dmitry Zavyalov <152815674+Hard-Working-Dimka@users.noreply.github.com>
2025-11-05 21:54:42 +0300

    add busses into window

--- server.py
+++ server.py
@@ -9,31 +9,48 @@ from trio_websocket import serve_websocket, ConnectionClosed
 BUSES = {}
 
 
-async def listen_browser(ws):
+async def listen_browser(ws, send_channel):
     while True:
         try:
             response = await ws.get_message()
             logging.debug(response)
-            print(response)
+            bounds = json.loads(response)
+            await send_channel.send(bounds)
+            await trio.sleep(5)
         except trio_websocket.ConnectionClosed:
             break
 
 
-async def send_new_location(ws):
+async def send_new_location(ws, receive_channel):
+    bounds = None
     while True:
+        try:
+            with trio.move_on_after(0.1) as move:
+                bounds = await receive_channel.receive()
+
+        except trio.Cancelled:
+            pass
+        if not bounds:
+            continue
+
         try:
             buses_location = {
                 "msgType": "Buses",
                 "buses": [
-                    {
-                        "busId": bus_id,
-                        "lat": bus_data['lat'],
-                        "lng": bus_data['lng'],
-                        "route": bus_data['route']
-                    }
-                    for bus_id, bus_data in BUSES.items()
+
                 ]
             }
+
+            for bus_id, bus_data in BUSES.items():
+                if is_inside(bounds['data'], bus_data['lat'], bus_data['lng'], ):
+                    buses_location['buses'].append(
+                        {
+                            "busId": bus_id,
+                            "lat": bus_data['lat'],
+                            "lng": bus_data['lng'],
+                            "route": bus_data['route']
+                        }
+                    )
 
             buses_location = json.dumps(buses_location)
             await ws.send_message(buses_location)
@@ -43,10 +60,20 @@ async def send_new_location(ws):
 
 
 async def talk_to_browser(request):
+    send_channel, receive_channel = trio.open_memory_channel(0)
     ws = await request.accept()
     async with trio.open_nursery() as nursery:
-        nursery.start_soon(listen_browser, ws)
-        nursery.start_soon(send_new_location, ws)
+        nursery.start_soon(listen_browser, ws, send_channel)
+        nursery.start_soon(send_new_location, ws, receive_channel)
+
+
+def is_inside(bounds, lat, lng):
+    s_lat = bounds['south_lat']
+    n_lng = bounds['north_lat']
+    w_lng = bounds['west_lng']
+    e_lng = bounds['east_lng']
+
+    return (s_lat <= lat) and (lat <= n_lng) and (w_lng <= lng) and (lng <= e_lng)
 
 
 async def update_current_location(request):
